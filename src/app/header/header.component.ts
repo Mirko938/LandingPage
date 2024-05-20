96f6d0440923e878fe4632a917a1f201d9dae0b2
Mirko <mirkodp938@gmail.com>
2024-05-20 11:50:14 +0200

    Router variable

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,25 +3,24 @@ import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
 import { PrimeNGConfig } from 'primeng/api';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
-import { LandingPageComponent } from '../landing-page/landing-page.component';
-import { LeadComponent } from '../lead/lead.component';
-
+import { Router, RouterModule} from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ToolbarModule, ButtonModule, CommonModule],
+  imports: [ToolbarModule, ButtonModule, CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
- routes!: Router;
-  constructor(private primengConfig: PrimeNGConfig) {}
+url: any[] = ["http://localhost:4200/landing-page"]
+  constructor(private primengConfig: PrimeNGConfig,
+              private router: Router
+  ) {}
   ngOnInit(): void {
     this.primengConfig.ripple = true;
   }
 
   toHome(): void {
-    location.href  = 'http://localhost:4200/landing-page';
+    this.router.navigate(this.url)
+    }
   }
-}
